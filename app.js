4ba061af72bb894e79223686e5f04dfdda178a3b
Thiago Assi <thiago-assi@outlook.com>
2021-03-30 21:28:10 -0300

    árvore de diretórios alterada

--- app.js
+++ app.js
@@ -12,24 +12,36 @@ app.listen(porta, () => {
   console.log(`Executando na porta ${porta}.`);
 });
 
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname + "/src/public/pages/login/index.html"));
+});
+
 app.get("/login", function(req, res) {
-  res.sendFile(path.join(__dirname + "/src/public/login.html"));
+  res.sendFile(path.join(__dirname + "/src/public/pages/login/index.html"));
 });
 
 app.get("/cadastro", function(req, res) {
-  res.sendFile(__dirname + "/src/public/cadastro.html");
+  res.sendFile(__dirname + "/src/public/pages/cadastro/index.html");
 });
 
 app.post("/logar", function(req, res) {
-  res.sendFile(path.join(__dirname + "/src/public/pagina-inicial.html"));
+  res.sendFile(path.join(__dirname + "/src/public/pages/pagina-inicial/index.html"));
 });
 
 app.post("/cadastrar", function(req, res) {
-  res.sendFile(__dirname + "/src/public/login.html");
+  res.sendFile(__dirname + "/src/public/pages/login/index.html");
 });
 
 app.get("/inicio", function(req, res) {
-  res.sendFile(__dirname + "/src/public/pagina-inicial.html");
+  res.sendFile(__dirname + "/src/public/pages/pagina-inicial/index.html");
+});
+
+app.get("/desafio", function(req, res) {
+  res.sendFile(__dirname + "/src/public/pages/desafio/index.html");
+});
+
+app.get("/recompensa", function(req, res) {
+  res.sendFile(__dirname + "/src/public/pages/recompensa/index.html");
 });
 
 app.use(express.static(__dirname + '/src/public'));
